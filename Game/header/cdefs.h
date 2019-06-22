6b61ed68b3ec801296e04991795589b21e2352d4
BeatrizKarine <48857427+BeatrizKarine@users.noreply.github.com>
2019-06-22 18:43:07 -0300

    Update cdefs.h

--- Game/header/cdefs.h
+++ Game/header/cdefs.h
@@ -44,35 +44,28 @@
 #define	__END_DECLS
 #endif
 
-/*
- * The __CONCAT macro is used to concatenate parts of symbol names, e.g.
- * with "#define OLD(foo) __CONCAT(old,foo)", OLD(foo) produces oldfoo.
- * The __CONCAT macro is a bit tricky -- make sure you don't put spaces
- * in between its arguments.  __CONCAT can also concatenate double-quoted
- * strings produced by the __STRING macro, but this only works with ANSI C.
- */
 #if defined(__STDC__) || defined(__cplusplus)
-#define	__P(protos)	protos		/* full-blown ANSI C */
+#define	__P(protos)	protos		
 #define	__CONCAT(x,y)	x ## y
 #define	__STRING(x)	#x
 
-#else	/* !(__STDC__ || __cplusplus) */
-#define	__P(protos)	()		/* traditional C preprocessor */
-#define	__CONCAT(x,y)	x/**/y
+#else	
+#define	__P(protos)	()		
+#define	__CONCAT(x,y)	x
 #define	__STRING(x)	"x"
 
 #ifdef __GNUC__
-#define	const		__const		/* GCC: ANSI C with -traditional */
+#define	const		__const		
 #define	inline		__inline
 #define	signed		__signed
 #define	volatile	__volatile
 
-#else	/* !__GNUC__ */
-#define	const				/* delete ANSI C keywords */
+#else
+#define	const			
 #define	inline
 #define	signed
 #define	volatile
-#endif	/* !__GNUC__ */
-#endif	/* !(__STDC__ || __cplusplus) */
+#endif	
+#endif
 
-#endif /* !_CDEFS_H_ */
+#endif 
