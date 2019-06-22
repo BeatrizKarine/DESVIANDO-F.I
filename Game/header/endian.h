fd640991fcf75b65d59f1980d046b1cfcd5e87c9
BeatrizKarine <48857427+BeatrizKarine@users.noreply.github.com>
2019-06-22 18:52:06 -0300

    Update endian.h

--- Game/header/endian.h
+++ Game/header/endian.h
@@ -21,23 +21,13 @@
 
 #include <features.h>
 
-/* Definitions for byte order, according to significance of bytes,
-   from low addresses to high addresses.  The value is what you get by
-   putting '4' in the most significant byte, '3' in the second most
-   significant byte, '2' in the second least significant byte, and '1'
-   in the least significant byte, and then writing down one digit for
-   each byte, starting with the byte at the lowest address at the left,
-   and proceeding to the byte with the highest address at the right.  */
 
 #define        __LITTLE_ENDIAN        1234
 #define        __BIG_ENDIAN        4321
 #define        __PDP_ENDIAN        3412
 
-/* This file defines `__BYTE_ORDER' for the particular machine.  */
 #include "endian.h"
 
-/* Some machines may need to use a different endianness for floating point
-   values.  */
 #ifndef __FLOAT_WORD_ORDER
 # define __FLOAT_WORD_ORDER __BYTE_ORDER
 #endif
@@ -57,7 +47,7 @@
 
 
 #ifdef __USE_BSD
-/* Conversion interfaces.  */
+
 # include <bits/byteswap.h>
 
 # if __BYTE_ORDER == __LITTLE_ENDIAN
@@ -93,4 +83,4 @@
 # endif
 #endif
 
-#endif        /* endian.h */
+#endif       
