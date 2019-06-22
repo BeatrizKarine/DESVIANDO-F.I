a76f1c6b0eca81849e54ff97a9df262b439cbcd0
BeatrizKarine <48857427+BeatrizKarine@users.noreply.github.com>
2019-06-22 18:31:28 -0300

    Update __locale_t.h

--- Game/header/__locale_t.h
+++ Game/header/__locale_t.h
@@ -15,20 +15,17 @@
    <http://www.gnu.org/licenses/>.  */
 #ifndef _BITS_TYPES___LOCALE_T_H
 #define _BITS_TYPES___LOCALE_T_H 1
-/* POSIX.1-2008: the locale_t type, representing a locale context
-   (implementation-namespace version).  This type should be treated
-   as opaque by applications; some details are exposed for the sake of
-   efficiency in e.g. ctype functions.  */
+
 struct __locale_struct
 {
-  /* Note: LC_ALL is not a valid index into this array.  */
-  struct __locale_data *__locales[13]; /* 13 = __LC_LAST. */
-  /* To increase the speed of this solution we add some special members.  */
+ 
+  struct __locale_data *__locales[13]; 
+ 
   const unsigned short int *__ctype_b;
   const int *__ctype_tolower;
   const int *__ctype_toupper;
-  /* Note: LC_ALL is not a valid index into this array.  */
+ 
   const char *__names[13];
 };
 typedef struct __locale_struct *__locale_t;
-#endif /* bits/types/__locale_t.h */
+#endif 
