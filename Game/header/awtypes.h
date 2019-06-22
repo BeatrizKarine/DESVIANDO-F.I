2d5a5b9e9e59f75f422d4e605f907dbf4b83387d
BeatrizKarine <48857427+BeatrizKarine@users.noreply.github.com>
2019-06-22 18:40:18 -0300

    Update awtypes.h

--- Game/header/awtypes.h
+++ Game/header/awtypes.h
@@ -19,16 +19,13 @@ typedef unsigned char UCHAR;
 typedef unsigned int UINT;
 typedef unsigned short USHORT;
 typedef unsigned long ULONG;
-// 1 BYTE = 8 bits
+
 typedef UCHAR BYTE;
-// 1 WORD = 16 bits = 2 BYTE
 typedef USHORT WORD;
-// 1 DWORD = 32 bits = 4 BYTE = 2 WORD
 typedef ULONG DWORD;
 typedef UCHAR bool_t, BOOL;
-// ASCII character
 typedef UCHAR ASCH;
-// Flags (2 bytes == 16 flags)
+
 typedef WORD FLAGS;
 
 typedef struct {
@@ -43,4 +40,4 @@ typedef struct {
 	int h;
 } RECT;
 
-#endif //AWTK_TYPES_H
+#endif 
