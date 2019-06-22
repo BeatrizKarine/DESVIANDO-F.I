dc3d415a08ed60fb8f5a6e0bb65ada36e3b273da
BeatrizKarine <48857427+BeatrizKarine@users.noreply.github.com>
2019-06-22 18:51:20 -0300

    Update dos.h

--- Game/header/dos.h
+++ Game/header/dos.h
@@ -1,43 +1,14 @@
-/*
- * dos.h
- *
- * DOS-specific functions and structures.
- *
- * This file is part of the Mingw32 package.
- *
- * Contributors:
- *  Created by J.J. van der Heijden <J.J.vanderHeijden@student.utwente.nl>
- *
- *  THIS SOFTWARE IS NOT COPYRIGHTED
- *
- *  This source code is offered for use in the public domain. You may
- *  use, modify or distribute it freely.
- *
- *  This code is distributed in the hope that it will be useful but
- *  WITHOUT ANY WARRANTY. ALL WARRANTIES, EXPRESS OR IMPLIED ARE HEREBY
- *  DISCLAIMED. This includes but is not limited to warranties of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- *
- * $Revision: 1.4 $
- * $Author: earnie $
- * $Date: 2002/06/14 15:12:54 $
- *
- */
-
 #ifndef	__STRICT_ANSI__
 
 #ifndef	_DOS_H_
 #define	_DOS_H_
-
-/* All the headers include this file. */
 #include "_mingw.h"
 
 #define __need_wchar_t
 #ifndef RC_INVOKED
 #include "stddef.h"
-#endif	/* Not RC_INVOKED */
+#endif	
 
-/* For DOS file attributes */
 #include "io.h"
 
 #ifndef RC_INVOKED
@@ -46,7 +17,7 @@
 extern "C" {
 #endif
 
-#ifndef __MSVCRT__ /* these are in CRTDLL, but not MSVCRT */
+#ifndef __MSVCRT__ 
 #ifndef __DECLSPEC_SUPPORTED
 extern unsigned int *_imp___basemajor_dll;
 extern unsigned int *_imp___baseminor_dll;
@@ -62,7 +33,7 @@ extern unsigned int *_imp___osmode_dll;
 #define _osminor (*_imp___osminor_dll)
 #define _osmode (*_imp___osmode_dll)
 
-#else /* __DECLSPEC_SUPPORTED */
+#else 
 
 __MINGW_IMPORT unsigned int _basemajor_dll;
 __MINGW_IMPORT unsigned int _baseminor_dll;
@@ -78,11 +49,10 @@ __MINGW_IMPORT unsigned int _osmode_dll;
 #define _osminor _osminor_dll
 #define _osmode _osmode_dll
 
-#endif /* __DECLSPEC_SUPPORTED */
-#endif /* ! __MSVCRT__ */
-
+#endif
+#endif 
 #ifndef _DISKFREE_T_DEFINED
-/* needed by _getdiskfree (also in direct.h) */
+
 struct _diskfree_t {
 	unsigned total_clusters;
 	unsigned avail_clusters;
@@ -102,9 +72,7 @@ unsigned _getdiskfree (unsigned, struct _diskfree_t *);
 }
 #endif
 
-#endif	/* Not RC_INVOKED */
+#endif	
+#endif	
 
-#endif	/* Not _DOS_H_ */
-
-#endif	/* Not __STRICT_ANSI__ */
-
+#endif	
