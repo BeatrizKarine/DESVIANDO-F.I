d177a5eb47a1c2f7b15f641dce1a06f5a97a6ca6
BeatrizKarine <48857427+BeatrizKarine@users.noreply.github.com>
2019-06-24 14:42:11 -0300

    Update byteswap.h

--- Game/header/byteswap.h
+++ Game/header/byteswap.h
@@ -23,7 +23,7 @@
 #include "features.h"
 #include "types.h"
 
-/* Swap bytes in 16-bit value.  */
+ 
 #define __bswap_constant_16(x)					\
   ((__uint16_t) ((((x) >> 8) & 0xff) | (((x) & 0xff) << 8)))
 
@@ -37,7 +37,7 @@ __bswap_16 (__uint16_t __bsx)
 #endif
 }
 
-/* Swap bytes in 32-bit value.  */
+ 
 #define __bswap_constant_32(x)					\
   ((((x) & 0xff000000u) >> 24) | (((x) & 0x00ff0000u) >> 8)	\
    | (((x) & 0x0000ff00u) << 8) | (((x) & 0x000000ffu) << 24))
@@ -52,7 +52,7 @@ __bswap_32 (__uint32_t __bsx)
 #endif
 }
 
-/* Swap bytes in 64-bit value.  */
+ 
 #define __bswap_constant_64(x)			\
   ((((x) & 0xff00000000000000ull) >> 56)	\
    | (((x) & 0x00ff000000000000ull) >> 40)	\
