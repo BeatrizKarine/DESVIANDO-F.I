5d959dec3d3130da076fc4cbccaa4642ca7d57e1
BeatrizKarine <48857427+BeatrizKarine@users.noreply.github.com>
2019-06-24 12:09:08 -0300

    Update stddef.h

--- Game/header/stddef.h
+++ Game/header/stddef.h
@@ -1,27 +1,24 @@
-/* stddef.h standard header */
+
 #ifndef _STDDEF
 #define _STDDEF
 #ifndef _YVALS
  #include "yvals.h"
-#endif /* _YMATH */
+#endif 
 _C_STD_BEGIN
 
-		/* macros */
 #ifndef NULL
  #define NULL	_NULL
-#endif /* NULL */
+#endif
 
 #ifndef offsetof
 #define offsetof(T, member)	((_CSTD _Sizet)&(((T *)0)->member))
-#endif /* offsetof */
-
-		/* type definitions */
+#endif 
  #if !defined(_PTRDIFF_T) && !defined(_PTRDIFFT)
   #define _PTRDIFF_T
   #define _PTRDIFFT
   #define _STD_USING_PTRDIFF_T
 typedef _Ptrdifft ptrdiff_t;
- #endif /* !defined(_PTRDIFF_T) && !defined(_PTRDIFFT) */
+ #endif 
 
  #if !defined(_SIZE_T) && !defined(_SIZET) \
 	&& !defined(_BSD_SIZE_T_DEFINED_)
@@ -30,24 +27,24 @@ typedef _Ptrdifft ptrdiff_t;
   #define _BSD_SIZE_T_DEFINED_
   #define _STD_USING_SIZE_T
 typedef _Sizet size_t;
- #endif /* !defined(_SIZE_T) etc. */
+ #endif 
 
 #ifndef _WCHART
  #define _WCHART
 typedef _Wchart wchar_t;
-#endif /* _WCHART */
+#endif 
 _C_STD_END
-#endif /* _STDDEF */
+#endif
 
  #if defined(_STD_USING) && defined(__cplusplus)
   #ifdef _STD_USING_PTRDIFF_T
 using _CSTD ptrdiff_t;
-  #endif /* _STD_USING_PTRDIFF_T */
+  #endif 
 
   #ifdef _STD_USING_SIZE_T
 using _CSTD size_t;
-  #endif /* _STD_USING_SIZE_T */
- #endif /* defined(_STD_USING) && defined(__cplusplus) */
+  #endif 
+ #endif 
 
 /*
  * Copyright (c) 1992-2002 by P.J. Plauger.  ALL RIGHTS RESERVED.
