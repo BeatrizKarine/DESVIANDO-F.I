45d8483a02ebe6d9dee98e92945882ae8a651141
BeatrizKarine <48857427+BeatrizKarine@users.noreply.github.com>
2019-06-24 12:14:29 -0300

    Update types.h

--- Game/header/types.h
+++ Game/header/types.h
@@ -1,7 +1,4 @@
-  
-/*	$OpenBSD: types.h,v 1.48 2019/02/09 04:54:11 guenther Exp $	*/
-/*	$NetBSD: types.h,v 1.29 1996/11/15 22:48:25 jtc Exp $	*/
-
+ 
 /*-
  * Copyright (c) 1982, 1986, 1991, 1993
  *	The Regents of the University of California.  All rights reserved.
@@ -54,19 +51,16 @@ typedef	unsigned short	u_short;
 typedef	unsigned int	u_int;
 typedef	unsigned long	u_long;
 
-typedef unsigned char	unchar;		/* Sys V compatibility */
-typedef	unsigned short	ushort;		/* Sys V compatibility */
-typedef	unsigned int	uint;		/* Sys V compatibility */
-typedef unsigned long	ulong;		/* Sys V compatibility */
+typedef unsigned char	unchar;		 
+typedef	unsigned short	ushort;		 
+typedef	unsigned int	uint;		 
+typedef unsigned long	ulong;		 
 
-typedef	__cpuid_t	cpuid_t;	/* CPU id */
-typedef	__register_t	register_t;	/* register-sized type */
-#endif /* __BSD_VISIBLE */
+typedef	__cpuid_t	cpuid_t;	 
+typedef	__register_t	register_t;	 
+#endif 
 
-/*
- * XXX The exact-width bit types should only be exposed if __BSD_VISIBLE
- *     but the rest of the includes are not ready for that yet.
- */
+ 
 #ifndef	__BIT_TYPES_DEFINED__
 #define	__BIT_TYPES_DEFINED__
 #endif
@@ -111,50 +105,47 @@ typedef	__int64_t		int64_t;
 typedef	__uint64_t		uint64_t;
 #endif
 
-/* BSD-style unsigned bits types */
+ 
 typedef	__uint8_t	u_int8_t;
 typedef	__uint16_t	u_int16_t;
 typedef	__uint32_t	u_int32_t;
 typedef	__uint64_t	u_int64_t;
 
-/* quads, deprecated in favor of 64 bit int types */
+ 
 typedef	__int64_t	quad_t;
 typedef	__uint64_t	u_quad_t;
 
 #if __BSD_VISIBLE
-/* VM system types */
+ 
 typedef __vaddr_t	vaddr_t;
 typedef __paddr_t	paddr_t;
 typedef __vsize_t	vsize_t;
 typedef __psize_t	psize_t;
-#endif /* __BSD_VISIBLE */
+#endif 
 
-/* Standard system types */
-typedef __blkcnt_t	blkcnt_t;	/* blocks allocated for file */
-typedef __blksize_t	blksize_t;	/* optimal blocksize for I/O */
-typedef	char *		caddr_t;	/* core address */
-typedef	__int32_t	daddr32_t;	/* 32-bit disk address */
-typedef	__int64_t	daddr_t;	/* 64-bit disk address */
-typedef	__dev_t		dev_t;		/* device number */
-typedef	__fixpt_t	fixpt_t;	/* fixed point number */
-typedef	__gid_t		gid_t;		/* group id */
-typedef	__id_t		id_t;		/* may contain pid, uid or gid */
-typedef	__ino_t		ino_t;		/* inode number */
-typedef	__key_t		key_t;		/* IPC key (for Sys V IPC) */
-typedef	__mode_t	mode_t;		/* permissions */
-typedef	__nlink_t	nlink_t;	/* link count */
-typedef	__rlim_t	rlim_t;		/* resource limit */
-typedef	__segsz_t	segsz_t;	/* segment size */
-typedef	__swblk_t	swblk_t;	/* swap offset */
-typedef	__uid_t		uid_t;		/* user id */
-typedef	__useconds_t	useconds_t;	/* microseconds */
-typedef	__suseconds_t	suseconds_t;	/* microseconds (signed) */
-typedef	__fsblkcnt_t	fsblkcnt_t;	/* file system block count */
-typedef	__fsfilcnt_t	fsfilcnt_t;	/* file system file count */
+typedef __blkcnt_t	blkcnt_t;	 
+typedef __blksize_t	blksize_t;	 
+typedef	char *		caddr_t;	 
+typedef	__int32_t	daddr32_t;	 
+typedef	__int64_t	daddr_t;	 
+typedef	__dev_t		dev_t;		 
+typedef	__fixpt_t	fixpt_t;	 
+typedef	__gid_t		gid_t;		 
+typedef	__id_t		id_t;		 
+typedef	__ino_t		ino_t;		 
+typedef	__key_t		key_t;		 
+typedef	__mode_t	mode_t;		 
+typedef	__nlink_t	nlink_t;	
+typedef	__rlim_t	rlim_t;		 
+typedef	__segsz_t	segsz_t;	 
+typedef	__swblk_t	swblk_t;	 
+typedef	__uid_t		uid_t;		 
+typedef	__useconds_t	useconds_t;	 
+typedef	__suseconds_t	suseconds_t;	 
+typedef	__fsblkcnt_t	fsblkcnt_t;	 
+typedef	__fsfilcnt_t	fsfilcnt_t;	 
 
-/*
- * The following types may be defined in multiple header files.
- */
+ 
 #ifndef	_CLOCK_T_DEFINED_
 #define	_CLOCK_T_DEFINED_
 typedef	__clock_t	clock_t;
@@ -195,32 +186,23 @@ typedef	__timer_t	timer_t;
 typedef	__off_t		off_t;
 #endif
 
-/*
- * These belong in unistd.h, but are placed here too to ensure that
- * long arguments will be promoted to off_t if the program fails to
- * include that header or explicitly cast them to off_t.
- */
+ 
 #if __BSD_VISIBLE && !defined(_KERNEL)
 __BEGIN_DECLS
 off_t	 lseek(int, off_t, int);
 int	 ftruncate(int, off_t);
 int	 truncate(const char *, off_t);
 __END_DECLS
-#endif /* __BSD_VISIBLE && !_KERNEL */
-
+#endif  
 #if __BSD_VISIBLE
-/* Major, minor numbers, dev_t's. */
+ 
 #define	major(x)	(((unsigned)(x) >> 8) & 0xff)
 #define	minor(x)	((unsigned)((x) & 0xff) | (((x) & 0xffff0000) >> 8))
 #define	makedev(x,y)	((dev_t)((((x) & 0xff) << 8) | ((y) & 0xff) | (((y) & 0xffff00) << 8)))
 #endif
 
 #if defined(__STDC__) && defined(_KERNEL)
-/*
- * Forward structure declarations for function prototypes.  We include the
- * common structures that cross subsystem boundaries here; others are mostly
- * used in the same place that the structure is defined.
- */
+ 
 struct	proc;
 struct	pgrp;
 struct	ucred;
@@ -234,29 +216,29 @@ struct	uio;
 #ifdef _KERNEL
 #if defined(__GNUC__) || \
 	(defined(__STDC_VERSION__) && __STDC_VERSION__ >= 199901)
-/* Support for C99: type _Bool is already built-in. */
+
 #define false	0
 #define true	1
 
 #else
-/* `_Bool' type must promote to `int' or `unsigned int'. */
+ 
 typedef enum {
 	false = 0,
 	true = 1
 } _Bool;
 
-/* And those constants must also be available as macros. */
+ 
 #define	false	false
 #define	true	true
 
 #endif
 
-/* User visible type `bool' is provided as a macro which may be redefined */
+ 
 #define bool _Bool
 
-/* Inform that everything is fine */
+ 
 #define __bool_true_false_are_defined 1
 
-#endif /* _KERNEL */
+#endif 
 
-#endif /* !_SYS_TYPES_H_ */
+#endif  
