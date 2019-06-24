45059d385705f2aa1a78f2fdce97e91bd99876e5
BeatrizKarine <48857427+BeatrizKarine@users.noreply.github.com>
2019-06-24 12:36:18 -0300

    Update wordsize.h

--- Game/header/wordsize.h
+++ Game/header/wordsize.h
@@ -1,4 +1,4 @@
-/* Determine the wordsize from the preprocessor defines.  */
+ 
 #if defined __x86_64__ && !defined __ILP32__
 # define __WORDSIZE        64
 #else
@@ -8,7 +8,7 @@
 #endif
 #ifdef __x86_64__
 # define __WORDSIZE_TIME64_COMPAT32        1
-/* Both x86-64 and x32 use the 64-bit system call interface.  */
+
 # define __SYSCALL_WORDSIZE                64
 #else
 # define __WORDSIZE_TIME64_COMPAT32        0
