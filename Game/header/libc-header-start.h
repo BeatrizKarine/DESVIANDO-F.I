3200e58f56d0e4353068f36f0a926de7f4dc1e14
BeatrizKarine <48857427+BeatrizKarine@users.noreply.github.com>
2019-06-24 12:04:33 -0300

    Update libc-header-start.h

--- Game/header/libc-header-start.h
+++ Game/header/libc-header-start.h
@@ -12,20 +12,13 @@
    You should have received a copy of the GNU Lesser General Public
    License along with the GNU C Library; if not, see
    <http://www.gnu.org/licenses/>.  */
-/* This header is internal to glibc and should not be included outside
-   of glibc headers.  Headers including it must define
-   __GLIBC_INTERNAL_STARTING_HEADER_IMPLEMENTATION first.  This header
-   cannot have multiple include guards because ISO C feature test
-   macros depend on the definition of the macro when an affected
-   header is included, not when the first system header is
-   included.  */
+
 #ifndef __GLIBC_INTERNAL_STARTING_HEADER_IMPLEMENTATION
 # error "Never include <bits/libc-header-start.h> directly."
 #endif
 #undef __GLIBC_INTERNAL_STARTING_HEADER_IMPLEMENTATION
 #include "features.h"
-/* ISO/IEC TR 24731-2:2010 defines the __STDC_WANT_LIB_EXT2__
-   macro.  */
+
 #undef __GLIBC_USE_LIB_EXT2
 #if (defined __USE_GNU                                                        \
      || (defined __STDC_WANT_LIB_EXT2__ && __STDC_WANT_LIB_EXT2__ > 0))
@@ -33,24 +26,21 @@
 #else
 # define __GLIBC_USE_LIB_EXT2 0
 #endif
-/* ISO/IEC TS 18661-1:2014 defines the __STDC_WANT_IEC_60559_BFP_EXT__
-   macro.  */
+
 #undef __GLIBC_USE_IEC_60559_BFP_EXT
 #if defined __USE_GNU || defined __STDC_WANT_IEC_60559_BFP_EXT__
 # define __GLIBC_USE_IEC_60559_BFP_EXT 1
 #else
 # define __GLIBC_USE_IEC_60559_BFP_EXT 0
 #endif
-/* ISO/IEC TS 18661-4:2015 defines the
-   __STDC_WANT_IEC_60559_FUNCS_EXT__ macro.  */
+
 #undef __GLIBC_USE_IEC_60559_FUNCS_EXT
 #if defined __USE_GNU || defined __STDC_WANT_IEC_60559_FUNCS_EXT__
 # define __GLIBC_USE_IEC_60559_FUNCS_EXT 1
 #else
 # define __GLIBC_USE_IEC_60559_FUNCS_EXT 0
 #endif
-/* ISO/IEC TS 18661-3:2015 defines the
-   __STDC_WANT_IEC_60559_TYPES_EXT__ macro.  */
+
 #undef __GLIBC_USE_IEC_60559_TYPES_EXT
 #if defined __USE_GNU || defined __STDC_WANT_IEC_60559_TYPES_EXT__
 # define __GLIBC_USE_IEC_60559_TYPES_EXT 1
